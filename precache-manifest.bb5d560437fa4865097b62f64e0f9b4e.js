65d1718b9149e30864e334b2d809d02f06a5b5bd
Wei Fang <lancefangw@gmail.com>
2019-09-10 22:40:21 -0700

    Updates

---
+++ precache-manifest.bb5d560437fa4865097b62f64e0f9b4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de6929de0f349ce5d738376f5980f94",
+    "url": "/salmonrun-rotation-static/index.html"
+  },
+  {
+    "revision": "4677c83e13223fd7cadc",
+    "url": "/salmonrun-rotation-static/static/css/main.21fc25e7.chunk.css"
+  },
+  {
+    "revision": "9f0086505671dc8f1f3d",
+    "url": "/salmonrun-rotation-static/static/js/2.6a2e5a7f.chunk.js"
+  },
+  {
+    "revision": "4677c83e13223fd7cadc",
+    "url": "/salmonrun-rotation-static/static/js/main.50e48071.chunk.js"
+  },
+  {
+    "revision": "538829fb3b54386f116a",
+    "url": "/salmonrun-rotation-static/static/js/runtime~main.d11c4b45.js"
+  }
+]);
